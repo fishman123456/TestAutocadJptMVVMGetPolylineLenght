823075893fccef0d8860dd5ec1d673170cfcbc15
fishman123456 <den9152@yandex.ru>
2024-05-15 23:30:22 +0700

    складывает длину полилиний -

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -30,6 +30,18 @@ namespace TestAutocadJptMVVMGetPolylineLenght
                 OnPropertyChanged(nameof(LineLength));
             }
         }
+        private List<string> _polyLenght;
+
+
+        public List<string> polyLenght
+        {
+            get { return _polyLenght; }
+            set
+            {
+                _polyLenght = value;
+                OnPropertyChanged(nameof(polyLenght));
+            }
+        }
 
         public ICommand GetLineLengthCommand => new RelayCommand(GetLineLength);
 
@@ -40,24 +52,33 @@ namespace TestAutocadJptMVVMGetPolylineLenght
             var doc = Application.DocumentManager.MdiActiveDocument;
             var db = doc.Database;
             var ed = doc.Editor;
+            List<string> polyLenght = new List<string>();
 
-            PromptEntityOptions opts = new PromptEntityOptions("\nSelect a line:");
-            opts.SetRejectMessage("\nSelected object is not a line.");
-           // opts.AddAllowedClass(typeof(Line), true);
+            PromptSelectionOptions opts = new PromptSelectionOptions();
+            opts.MessageForAdding = "Выберите обьекты: ";
+            PromptSelectionResult res = ed.GetSelection(opts);
+            // Do nothing if selection is unsuccessful
 
-            PromptEntityResult res = ed.GetEntity(opts);
-            if (res.Status != PromptStatus.OK) return;
+            if (res.Status != PromptStatus.OK)
+                return ;
+            SelectionSet selSet = res.Value;
+            // добавляем в массив выбранные обьекты
+            ObjectId[] idArray = selSet.GetObjectIds();
 
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
-                var line = tr.GetObject(res.ObjectId, OpenMode.ForRead) as Polyline;
-                if (line != null)
+                foreach (ObjectId id in idArray)
                 {
-                    LineLength = line.Length/1000;
+                    doc.LockDocument();
+                    var ent = tr.GetObject(id, OpenMode.ForWrite) as Polyline;
+                    if (ent != null )
+                    {
+                        polyLenght.Add(ent.Length.ToString());
+                        LineLength += ent.Length;
+                    }
                 }
-
                 tr.Commit();
-            }
+            }   
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
